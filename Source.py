266d339589fc5bc42f240636e0ba3b08679b257c
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-05-30 19:22:09 +0200

    modularized source, propagator and simulation classes

--- Source.py
+++ Source.py
@@ -6,13 +6,6 @@ from numba.typed import List
 from numba.experimental import jitclass
 
 class Source():
-    # generate particles
-    @jit(nopython=True)
-    def source():
-        particles = List()
-        for j in range(nr_particles):
-            direction = List()
-            random_values = np.random.randint(low=0, high=1, size=3)*2-1
-            [direction.append(x) for x in random_values]
-            particles.append(particle(gyro_radius, free_mean_path_para, free_mean_path_perp, direction))
-        return particles
+    def __init__(self, nr_particles, position):
+        self.nr_particles = nr_particles
+        self.position = position
