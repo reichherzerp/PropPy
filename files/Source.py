7e0d87ba8cc746920a846f37590a4642452da935
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-08-02 13:34:21 +0200

    added function to initialize source and delete all prevoius particles
    Former-commit-id: 672c7606050eebfd37bf898f92790b585d38a2ad

--- files/Source.py
+++ files/Source.py
@@ -2,10 +2,16 @@ from files.Particle import Particle
 
 class Source():
     def __init__(self, gyro_radius, mean_free_path, pos, nr_particles):
+        self.gyro_radius = gyro_radius
+        self.mean_free_path = mean_free_path
+        self.pos = pos
+        self.nr_particles = nr_particles
         print('source')
         self.particles = []
-        for i in range(nr_particles):
+
+    def init_source(self):
+        self.particles = []
+        for i in range(self.nr_particles):
             particle_id = i
-            p = Particle(particle_id, gyro_radius, mean_free_path, pos)
+            p = Particle(particle_id, self.gyro_radius, self.mean_free_path, self.pos[:])
             self.particles.append(p)
-            
