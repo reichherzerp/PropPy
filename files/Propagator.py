2ec48a44da946a8de7ae182857067169ae0a90f7
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-08-02 11:50:48 +0200

    added properties of Propagator
    Former-commit-id: 166a5c232239a3057d952f3681f3ec860b35a79d

--- files/Propagator.py
+++ files/Propagator.py
@@ -1,3 +1,13 @@
+from numba import jit, b1, float32, int32
+import numpy as np
+from numba.experimental import jitclass
+
+simulation_spec = [
+    ('isotropic', b1)
+]
+
+@jitclass(simulation_spec)
 class Propagator():
     def __init__(self):
         print('Propagator initialized')
+        self.isotropic = False
