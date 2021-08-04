11daa9f80afc94cdfa56722b7b0a1b866c17f0a6
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-08-04 16:50:17 +0200

    compute the travelled distance correcly, also in substeps
    Former-commit-id: c6cd5f3aa4e795877fb4414710a6cec367ac9882

--- files/Propagator.py
+++ files/Propagator.py
@@ -7,6 +7,7 @@ simulation_spec = [
     ('cylindrical', b1),
     ('nr_steps', int32),
     ('dimensions', int32),
+    ('background_direction', int32),
     ('step_distance', float32),
     ('step_size', float32),
     ('gyro_radius_eff', float32),
@@ -26,13 +27,14 @@ simulation_spec = [
 class Propagator():
     def __init__(self, nr_steps, step_size, mean_free_path):
         print('Propagator initialized')
-        self.speed = 3*10**8 # [m^2/s]
+        self.speed = 2.998*10**8 # [m/s]
         self.cartesian = False
         self.cylindrical = True
         self.nr_steps = nr_steps
-        self.step_size = step_size
+        self.step_size = step_size # [m]
         self.dimensions = 3
         self.pitch_angle_const = True
+        self.background_direction = 2 # direction of a background field
         self.set_prop(mean_free_path)
         
 
@@ -103,6 +105,8 @@ class Propagator():
         else:
             print('coordinates: Cylindrical coordinates')
         print('dimensions: ', self.dimensions)
+        if self.dimensions < 3:
+            print(self.dimensions, ' dimensions not supoorted yet!')
         if self.pitch_angle_const:
             print('pitch angle: constant')
         else:
@@ -118,32 +122,31 @@ class Propagator():
 
     def change_direction(self, direction, pitch_angle):
         ### change in direction happens with a propability that is defined by the 
-        ### mean free path
+        ### mean free path and stored in self.prob
         for p in range(self.dimensions):
             if np.random.random() < self.prob[p]:
                 if self.pitch_angle_const == False and p == self.dimensions-1:
                     if np.random.random() < 0.5:
                         direction[p] = -1*direction[p]
-                    pitch_angle = pitch_angle + direction[2] * 0.1
+                    pitch_angle = pitch_angle + direction[self.dimensions-1] * 0.1
                 else:
                     direction[p] = -1*direction[p]
         return direction, pitch_angle
 
 
     def move_substep(self, pos, direction, phi, pitch_angle, distance, gyro_radius, s):
-        distance = distance + self.step_size / self.dimensions
         self.gyro_radius_eff = gyro_radius / 3**0.5 # correcting for moving in rho direction (perp to phi) --> gyration increases by 2**0.5, which is why we have to divide here.
         if self.cartesian:
             # cartesian coordinates -> move in x, y and z directions
-            pos = self.move_cartesian(pos, direction, pitch_angle, s)
+            pos, distance = self.move_cartesian(pos, direction, pitch_angle, distance, s)
         else:
             # cylindrical coordinates -> move in phi, rho and z directions
             if s == 0:
-                pos, phi = self.move_phi(pos, direction, phi, pitch_angle)
+                pos, phi, distance = self.move_phi(pos, direction, phi, pitch_angle, distance)
             if s == 1:
-                pos, phi = self.move_rho(pos, direction, phi, pitch_angle)
+                pos, phi, distance = self.move_rho(pos, direction, phi, pitch_angle, distance)
             if s == 2:
-                pos = self.move_cartesian(pos, direction, pitch_angle, 2)
+                pos, distance = self.move_cartesian(pos, direction, pitch_angle, distance, 2)
         data = {
             'distance': distance, 
             'phi': phi,
@@ -153,8 +156,8 @@ class Propagator():
         return data
          
             
-    def move_cartesian(self, pos, direction, pitch_angle, s):
-        if s == 2:
+    def move_cartesian(self, pos, direction, pitch_angle, distance, s):
+        if s == self.background_direction:
             distance_s = self.step_size * np.cos(pitch_angle)
             if self.pitch_angle_const == False:
                 pos[s] = pos[s] + distance_s
@@ -163,27 +166,32 @@ class Propagator():
         else:
             distance_s = self.step_size * np.sin(pitch_angle) / 2**0.5
             pos[s] = pos[s] + direction[s] * distance_s
-        return self.position(pos)
+        distance = distance + distance_s
+        return self.position(pos), distance
         
         
-    def move_phi(self, pos, direction, phi, pitch_angle):
+    def move_phi(self, pos, direction, phi, pitch_angle, distance):
         phi_old = phi
+        distance_s = self.step_size * np.sin(pitch_angle) / 2**0.5
+        distance = distance + distance_s
         delta_phi = self.compute_delta_phi(pitch_angle)
         phi = phi_old + delta_phi * direction[0]
         chi_x_1 = self.gyro_radius_eff * (np.cos(phi) - np.cos(phi_old))
         chi_y_1 = self.gyro_radius_eff * (np.sin(phi) - np.sin(phi_old))
         pos[0] = pos[0] + chi_x_1
         pos[1] = pos[1] + chi_y_1
-        return self.position(pos), phi
+        return self.position(pos), phi, distance
 
                       
-    def move_rho(self, pos, direction, phi, pitch_angle):
+    def move_rho(self, pos, direction, phi, pitch_angle, distance):
+        distance_s = self.step_size * np.sin(pitch_angle) / 2**0.5
+        distance = distance + distance_s
         delta_rho = self.step_size * np.sin(pitch_angle) / 2**0.5
         chi_x_2 = np.cos(phi) * direction[1] * delta_rho
         chi_y_2 = np.sin(phi) * direction[1] * delta_rho
         pos[0] = pos[0] + chi_x_2
         pos[1] = pos[1] + chi_y_2
-        return self.position(pos), phi
+        return self.position(pos), phi, distance
 
 
     def compute_delta_phi(self, pitch_angle):
