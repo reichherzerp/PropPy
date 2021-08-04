27ce3bffac42fe16497a5a63f1f489f8f3ae49f5
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-08-04 17:38:30 +0200

    finished pitch angle implementation
    Former-commit-id: cbf38cd348610d80dbe6e757addfaf01aac06a5d

--- files/Particle.py
+++ files/Particle.py
@@ -48,8 +48,8 @@ class Particle():
         simulation_data.append([self.particle_id, 0, self.distance, self.pos[0], self.pos[1], self.pos[2], -1.0, self.dimensions-1])
         self.pos = np.array([self.pos_start[0], self.pos_start[1], self.pos_start[2]], dtype=np.float32)
         for i in range(1, propagator.nr_steps): 
-            self.direction, self.pitch_angle = propagator.change_direction(self.direction, self.pitch_angle)
-            pitch_angle = propagator.change_pitch_angle(pitch_angle)
+            self.direction = propagator.change_direction(self.direction)
+            self.pitch_angle = propagator.change_pitch_angle(self.pitch_angle)
             self.pos_prev = self.pos 
             for substep in range(self.dimensions):
                 self.propagate(propagator, substep)
