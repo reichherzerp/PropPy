4879ff1a2b61fd76ba82b6dc6942667b3c28854a
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-08-08 09:25:14 +0200

    implemented usage of ParticleState in Particle class for all parameter occurances
    Former-commit-id: 9d4451d58c3d6df3d8ed0b99537eac900624723f

--- rwpropa/rwpropa/particle.py
+++ rwpropa/rwpropa/particle.py
@@ -5,7 +5,9 @@ from .observer import Observer
 from .propagator import Propagator
 
 
-particle_spec = [
+
+
+particle_state_spec = [
     ('step_distance', float32),
     ('chi_isotropic', float32),
     ('speed', float32),
@@ -25,38 +27,7 @@ particle_spec = [
     ('propagator', Propagator.class_type.instance_type),
 ]
 
-@jitclass(particle_spec)
-class Particle():
-    def __init__(self, particle_id, gyro_radius, pos, phi, pitch_angle, dimensions):
-        ParticleState(particle_id, gyro_radius, pos, phi, pitch_angle, dimensions)
-        
-        
-    def simulate(self, observer, propagator):
-        simulation_data = []
-    
-        simulation_data.append([self.particle_id, 0, self.distance, self.pos[0], self.pos[1], self.pos[2], -1.0, self.dimensions-1])
-        self.pos = np.array([self.pos_start[0], self.pos_start[1], self.pos_start[2]], dtype=np.float32)
-        for i in range(1, propagator.nr_steps): 
-            self.direction = propagator.change_direction(self.direction)
-            self.pitch_angle = propagator.change_pitch_angle(self.pitch_angle)
-            self.pos_prev = self.pos 
-            for substep in range(self.dimensions):
-                self.propagate(propagator, substep)
-                observation = observer.observe(i, substep, self.distance, self.pos, self.particle_id, self.phi, self.pitch_angle)
-                if observation is not None:
-                    simulation_data.append(observation)
-                
-        return simulation_data
-
-
-    def propagate(self, propagator, substep):
-        data = propagator.move_substep(self.pos, self.direction, self.phi, self.pitch_angle, self.distance, self.gyro_radius, substep)
-        self.distance = data['distance']
-        self.phi = data['phi']
-        self.pos = data['pos'] 
-
-
-@jitclass(particle_spec)
+@jitclass(particle_state_spec)
 class ParticleState():
     def __init__(self, particle_id, gyro_radius, pos, phi, pitch_angle, dimensions):
         self.speed = 3*10**8 # [m^2/s]
@@ -71,3 +42,58 @@ class ParticleState():
         self.direction = np.array([1.0, 1.0, 1.0], dtype=np.float32)
         self.phi = phi
         self.pitch_angle = pitch_angle
+
+        
+
+particle_spec = [
+    ('step_distance', float32),
+    ('chi_isotropic', float32),
+    ('speed', float32),
+    ('isotropic', b1),
+    ('distance', float32),
+    ('gyro_radius', float32),
+    ('phi', float32),
+    ('pitch_angle', float32),
+    ('particle_id', int32),
+    ('dimensions', int32),
+    ('pos_start', float32[:]),
+    ('pos', float32[:]),
+    ('pos_prev', float32[:]),
+    ('direction', float32[:]),
+    ('prob', float32[:]),
+    ('observer', Observer.class_type.instance_type),
+    ('propagator', Propagator.class_type.instance_type),
+    ('particle_state', ParticleState.class_type.instance_type),
+]
+
+@jitclass(particle_spec)
+class Particle():
+    def __init__(self, particle_id, gyro_radius, pos, phi, pitch_angle, dimensions):
+        self.particle_state = ParticleState(particle_id, gyro_radius, pos, phi, pitch_angle, dimensions)
+        
+        
+    def simulate(self, observer, propagator):
+        simulation_data = []
+    
+        simulation_data.append([self.particle_state.particle_id, 0, self.particle_state.distance, self.particle_state.pos[0], self.particle_state.pos[1], self.particle_state.pos[2], -1.0, self.particle_state.dimensions-1])
+        self.particle_state.pos = np.array([self.particle_state.pos_start[0], self.particle_state.pos_start[1], self.particle_state.pos_start[2]], dtype=np.float32)
+        for i in range(1, propagator.nr_steps): 
+            self.particle_state.direction = propagator.change_direction(self.particle_state.direction)
+            self.particle_state.pitch_angle = propagator.change_pitch_angle(self.particle_state.pitch_angle)
+            self.particle_state.pos_prev = self.particle_state.pos 
+            for substep in range(self.particle_state.dimensions):
+                self.propagate(propagator, substep)
+                observation = observer.observe(i, substep, self.particle_state.distance, self.particle_state.pos, self.particle_state.particle_id, self.particle_state.phi, self.particle_state.pitch_angle)
+                if observation is not None:
+                    simulation_data.append(observation)
+                
+        return simulation_data
+
+
+    def propagate(self, propagator, substep):
+        data = propagator.move_substep(self.particle_state.pos, self.particle_state.direction, self.particle_state.phi, self.particle_state.pitch_angle, self.particle_state.distance, self.particle_state.gyro_radius, substep)
+        self.particle_state.distance = data['distance']
+        self.particle_state.phi = data['phi']
+        self.particle_state.pos = data['pos'] 
+
+
