acf3b824672fbfce0eb836a2df652d7f34ad1938
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-08-11 09:33:44 +0200

    use energy instead of gyro radius in simulation setup in tutorials
    Former-commit-id: 3eca97e143934b8913f08ad08d9bcac01328b45c

--- rwpropa/rwpropa/particle_state.py
+++ rwpropa/rwpropa/particle_state.py
@@ -8,7 +8,7 @@ particle_state_spec = [
     ('step_distance', float32),
     ('chi_isotropic', float32),
     ('speed', float32),
-    ('energy', float32)
+    ('energy', float32),
     ('isotropic', b1),
     ('distance', float32),
     ('gyro_radius', float32),
@@ -26,16 +26,16 @@ particle_state_spec = [
 
 @jitclass(particle_state_spec)
 class ParticleState():
-    def __init__(self, particle_id, gyro_radius, pos, phi, pitch_angle, dimensions):
+    def __init__(self, particle_id, energy, pos, phi, pitch_angle, dimensions):
         self.speed = 3*10**8 # [m^2/s]
-        self.gyro_radius = gyro_radius
+        self.energy = energy
+        self.gyro_radius = 0.0
         self.particle_id = particle_id
         self.isotropic = False
         self.dimensions = dimensions
         self.distance = 0.0
         self.substep = 0
         self.step = 0
-        self.energy = 3*10**15 # eV
         self.pos_start = pos[:]
         self.pos = pos[:]
         self.pos_prev = self.pos[:]
