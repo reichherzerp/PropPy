b1d355c17bf1ac95233dd28e6245b8164061d2f7
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-08-30 12:18:15 +0200

    implemented unittest for isotropic point source
    Former-commit-id: 74159e3b335ffc54fd1beb9c7a4180eac760a119

--- rwpropa/tests.py
+++ rwpropa/tests.py
@@ -8,7 +8,7 @@ import rwpropa as rw
 class TestSource(unittest.TestCase):
 
     def test_point_source_oriented(self):
-        print('-> test_point_source_oriented')
+        print('\n-> test_point_source_oriented')
         energy = 10**10 #eV
         pos = [0,0,0]
         nr_particles = 10**2
@@ -20,6 +20,10 @@ class TestSource(unittest.TestCase):
         # test if the energy of a generated particle is correct
         self.assertEqual(energy, point_source_oriented.particles[0].ps.energy)
         # check the position of the particles
+        expected_pos = list(np.array(pos, dtype=np.float32))
+        actual_pos = point_source_oriented.particles[0].ps.pos.tolist()
+        self.assertListEqual(expected_pos, actual_pos)
+        # check the direction of the particles
         precision = 4
         expected_direction = list(np.around(np.array([
             np.cos(phi)*np.sin(pitch_angle), 
@@ -28,6 +32,22 @@ class TestSource(unittest.TestCase):
         ], dtype=np.float32), precision))
         actual_direction = list(np.around(np.array(point_source_oriented.particles[0].ps.direction), precision))
         self.assertListEqual(expected_direction, actual_direction)
+
+    
+    def test_point_source_isotropic(self):
+        print('\n-> test_point_source_isotropic')
+        energy = 10**10 #eV
+        pos = [0,0,0]
+        nr_particles = 10**2
+        point_source_oriented = rw.PointSourceIsotropic(energy, pos, nr_particles)
+        # test if the number of generated particles is correct
+        self.assertEqual(nr_particles, len(point_source_oriented.particles))
+        # test if the energy of a generated particle is correct
+        self.assertEqual(energy, point_source_oriented.particles[0].ps.energy)
+        # check the position of the particles
+        expected_pos = list(np.array(pos, dtype=np.float32))
+        actual_pos = point_source_oriented.particles[0].ps.pos.tolist()
+        self.assertListEqual(expected_pos, actual_pos)
         
 
     
