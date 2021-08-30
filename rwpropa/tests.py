c6ddd08c7fdaeee6d93c3330a1376559b79ca396
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-08-30 10:51:39 +0200

    test if the energy of a generated particle is correct
    Former-commit-id: 652e65a4ad6902f9425bbe253f64929fd0ea0e46

--- rwpropa/tests.py
+++ rwpropa/tests.py
@@ -6,12 +6,17 @@ import rwpropa as rw
 class TestSource(unittest.TestCase):
 
     def test_point_source_oriented(self):
+        print('-> test_point_source_oriented')
         energy = 10**10 #eV
         pos = [0,0,0]
         nr_particles = 10**2
-        pitch_angle = 1
+        pitch_angle = 0
         phi = 0
         point_source_oriented = rw.PointSourceOriented(energy, pos, nr_particles, pitch_angle, phi)
-        #self.assertEqual(
+        # test if the number of generated particles is correct
+        self.assertEqual(nr_particles, len(point_source_oriented.particles))
+        # test if the energy of a generated particle is correct
         self.assertEqual(energy, point_source_oriented.particles[0].ps.energy)
-        #len(point_source_oriented.particles)
+        
+
+    
