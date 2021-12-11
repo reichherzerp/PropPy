f0150c213c15717f62fd76b7dfb97227db2ede0f
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-11 10:45:07 +0100

    create parameter file for simulaiton to track used parameters
    Former-commit-id: 0f3a1948acd71816f09e69258f5c2303276d3cc0

--- rwpropa/job.sh
+++ rwpropa/job.sh
@@ -2,7 +2,7 @@
 file_name="/home/patrick/Documents/rwpropa/test/test_new_"
 nr=5
 for i in `seq 1 $nr`; 
-do python3 server_script.py "$file_name${i}"& 
+do python3 server_script.py $file_name ${i}& 
 done
 wait
 python3 server_merge_script.py $file_name $nr
--- rwpropa/server_script.py
+++ rwpropa/server_script.py
@@ -7,7 +7,9 @@ import rwpropa as rw
 import sys
 
 
-def simulate(file_name):
+def simulate(file_name, i):
+    file_name_i = file_name + str(i)
+    parameters = {'nr_particles': 10**2, 'radius': 10**14 }
     nr_particles = 10**2 
     radius = 10**14 
     energy = 10**15
@@ -16,12 +18,20 @@ def simulate(file_name):
     step_size = 1*10**12
     sim = rw.PlasmoidSimulation(nr_particles = nr_particles, radius = radius, energy = energy, nr_steps = nr_steps, diffusion_coefficient = diffusion_coefficient, step_size = step_size)
     start = time.time()
-    sim.simulate(file_name)
-    print('finished: ', file_name)
+    sim.simulate(file_name_i)
+    print('finished: ', i)
     end = time.time()
-    print("Elapsed (after compilation) = %s" % (end - start))
+    print('Elapsed (after compilation) = %s' % (end - start))
+
+    # open file for writing
+    f = open(file_name+'_parameters.txt','w')
+    # write file
+    f.write( str(parameters) )
+    # close file
+    f.close()
 
 
 if __name__ == '__main__':
     file_name = str(sys.argv[1])
-    simulate(file_name)
+    i = str(sys.argv[2])
+    simulate(file_name, i)
