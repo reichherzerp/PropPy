72a08bb2a2397ba0ac193d7961391aea2d35c500
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-12 09:53:09 +0100

    restructured project and added server folder
    Former-commit-id: c0c66378ceeb87424fc1d0f773ecf35cac67b81f

--- rwpropa/job.sh
+++ rwpropa/job.sh
@@ -2,8 +2,8 @@
 file_name="/home/patrick/Documents/rwpropa/test/test_"
 nr=5
 for i in `seq 1 $nr`; 
-do python3 server_script.py $file_name ${i} ${nr}& 
+do python3 server/server_script.py $file_name ${i} ${nr}& 
 done
 wait
-python3 server_merge_script.py $file_name $nr
+python3 server/server_merge_script.py $file_name $nr
 exit
