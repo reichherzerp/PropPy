443878c7efb114fb03716837c5bd9dc9d5eb6dbb
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-21 18:38:47 +0100

    add source to modulelist
    Former-commit-id: 93695f435ebf3440583bf5a7598daa893a13f84f

--- comparison/sim.py
+++ comparison/sim.py
@@ -3,3 +3,11 @@ import numpy as np
 
 
 sim = crp.ModuleList()
+
+# source settings
+# A point source at the origin is isotropically injecting protons.
+source = crp.Source()
+source.add(crp.SourcePosition(crp.Vector3d(0)))
+source.add(crp.SourceParticleType(crp.nucleusId(1, 1)))
+source.add(crp.SourceEnergy(3*10**16*crp.eV))
+source.add(crp.SourceIsotropicEmission())
