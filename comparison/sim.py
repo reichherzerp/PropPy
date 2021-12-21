d588ccf08d8c6be4a85bb947c1fc458dbde21b2e
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-21 18:39:27 +0100

    added plane wave turbulence to simulation
    Former-commit-id: 1a159ac94371e63e48535147ee9799ebb90a6963

--- comparison/sim.py
+++ comparison/sim.py
@@ -11,3 +11,13 @@ source.add(crp.SourcePosition(crp.Vector3d(0)))
 source.add(crp.SourceParticleType(crp.nucleusId(1, 1)))
 source.add(crp.SourceEnergy(3*10**16*crp.eV))
 source.add(crp.SourceIsotropicEmission())
+
+# magnetic field                                                                                              
+b_field = crp.MagneticFieldList()
+brms = 3*10**6*crp.muG
+b_background = 10**7*crp.muG
+l_max = 5*10**11 # [m]
+l_min = 5*10**9 # [m]
+turbulence_spectrum = crp.SimpleTurbulenceSpectrum(brms, l_min, l_max)
+turbulence = crp.PlaneWaveTurbulence(turbulence_spectrum, Nm = 400)
+b_field.addField(turbulence)
