5709779452c2b8e8e55ac81947512774ab55b911
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-21 20:33:38 +0100

    added propagation routine from crpropa
    Former-commit-id: 1d3323f55f9e404b8020ddcdbda8ccc2244f7a07

--- comparison/sim.py
+++ comparison/sim.py
@@ -23,3 +23,11 @@ turbulence = crp.PlaneWaveTurbulence(turbulence_spectrum, Nm = 400)
 b_field.addField(turbulence)
 regular_field = crp.UniformMagneticField(crp.Vector3d(0,0,1)*b_background)
 b_field.addField(regular_field)
+
+# propagation
+step_size = 10**10 #m
+prop_bp = crp.PropagationBP(b_field, step_size)
+sim.add(prop_bp)
+t_max = 10**5*step_size
+maxTra = crp.MaximumTrajectoryLength(t_max)
+sim.add(maxTra)
