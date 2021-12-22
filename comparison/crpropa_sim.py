7798dc5616233e9129269b8011e099e47ac9f3da
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-22 16:36:31 +0100

    refactored simulation into function with some arguments
    Former-commit-id: 6e721712bbd4b6121d21981cf271e3848d23a0c6

--- comparison/crpropa_sim.py
+++ comparison/crpropa_sim.py
@@ -2,7 +2,7 @@ import crpropa as crp
 import numpy as np
 
 
-def crpropa_sim():
+def crpropa_sim(step_size = 10**11):
     sim = crp.ModuleList()
 
     # source settings
@@ -13,9 +13,9 @@ def crpropa_sim():
     source.add(crp.SourceEnergy(3*10**15*crp.eV))
     source.add(crp.SourceIsotropicEmission())
 
-    # magnetic field                                                                                              
+    # magnetic field 
     b_field = crp.MagneticFieldList()
-    brms = 3*10**6*crp.muG
+    brms = 10**6*crp.muG
     b_background = 10**7*crp.muG
     l_max = 5*10**11 # [m]
     l_min = 5*10**9 # [m]
@@ -23,10 +23,9 @@ def crpropa_sim():
     turbulence = crp.PlaneWaveTurbulence(turbulence_spectrum, Nm = 100)
     b_field.addField(turbulence)
     regular_field = crp.UniformMagneticField(crp.Vector3d(0,0,1)*b_background)
-    b_field.addField(regular_field)
+    #b_field.addField(regular_field)
 
     # propagation
-    step_size = 10**11 #m
     prop_bp = crp.PropagationBP(b_field, step_size)
     sim.add(prop_bp)
     t_max = 10**4*step_size
@@ -34,7 +33,7 @@ def crpropa_sim():
     sim.add(maxTra)
 
     # output
-    output_lin = crp.TextOutput('sim_result.txt', crp.Output.Trajectory3D)
+    output_lin = crp.TextOutput('sim_result'+str(step_size/10**11)+'.txt', crp.Output.Trajectory3D)
     output_lin.enable(output_lin.SerialNumberColumn)
     output_lin.enable(output_lin.SourceDirectionColumn)
 
