995978f3380cc79c354826c3e5bcfa725f097067
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-22 16:48:59 +0100

    implemented convergence tests for step size
    Former-commit-id: cac107c4da4db2513f9292838c310477c69ddf34

--- comparison/crpropa_sim.py
+++ comparison/crpropa_sim.py
@@ -1,5 +1,13 @@
 import crpropa as crp
 import numpy as np
+import pandas as pd
+pc = 3.086*10**16
+Mpc = 10**6*pc
+c = 2.99792*10**8
+N_time = 100
+number_particles = 10**3
+N = number_particles
+file_name = 'sim_result_'
 
 
 def crpropa_sim(step_size = 10**11):
@@ -33,7 +41,7 @@ def crpropa_sim(step_size = 10**11):
     sim.add(maxTra)
 
     # output
-    output_lin = crp.TextOutput('sim_result'+str(step_size/10**11)+'.txt', crp.Output.Trajectory3D)
+    output_lin = crp.TextOutput(file_name+str(step_size/10**11)+'.txt', crp.Output.Trajectory3D)
     output_lin.enable(output_lin.SerialNumberColumn)
     output_lin.enable(output_lin.SourceDirectionColumn)
 
@@ -49,3 +57,51 @@ def crpropa_sim(step_size = 10**11):
     sim.setShowProgress(True)
     number_particles = 10**3
     sim.run(source, number_particles, True)
+
+
+def load_data(x):
+    dataI = pd.read_csv(x, names=['D', 'SN', 'ID', 'E', 'X', 'Y', 'Z', 'Px', 'Py', 'Pz', 'SN0', 'P0x', 'P0y', 'P0z', 'SN1'], delimiter='\t', comment='#', usecols=["D", "X", "Y", "Z", "\
+SN"])
+
+    ### Convert data from Mpc to meters                                            
+    dataI.X = dataI.X * Mpc
+    dataI.Y = dataI.Y * Mpc
+    dataI.Z = dataI.Z * Mpc
+    dataI.D = dataI.D * Mpc
+    ### Calculate Diffusion Coefficients
+    dataI['X2D'] = dataI.X**2 / (dataI.D) * c / 2.
+    dataI['Y2D'] = dataI.Y**2 / (dataI.D) * c / 2.
+    dataI['Z2D'] = dataI.Z**2 / (dataI.D) * c / 2.
+    dataI['R2D'] = (dataI.X**2+dataI.Y**2+dataI.Z**2)**0.5
+    ### Number of Gyrations                                                            
+    dataI.D = dataI.D #/ (2 * math.pi * r)
+    print(len(dataI.D.values.tolist()))
+    
+    dataI = dataI.sort_values('D')
+    return dataI
+
+
+def diffusion_coefficients(data):
+    kappa_xx = []
+    kappa_yy = []
+    kappa_zz = []
+    kappa_rr = []
+    L = return_L(data)
+    for l in L:
+        dataI = data[data['D'] == l]
+        kappa_xx.append(np.mean(dataI.X2D.values + dataI.Y2D.values)/2.0)
+        kappa_zz.append(np.mean(dataI.Z2D.values))
+    return kappa_xx, kappa_zz
+
+def return_L(dataI):
+    L = list(set(dataI.D.values.tolist()))
+    return sorted(L)
+
+
+def analyze_agn(step_size, file_name_output):
+    dataLin = load_data(file_name+str(step_size/10**11)+'.txt')
+    kappa_perp, kappa_para = diffusion_coefficients(dataLin)
+    L = return_L(dataLin)
+    np.save(file_name_output+str(step_size/10**11)+'_d', np.array(L))
+    np.save(file_name_output+str(step_size/10**11)+'_kappa_perp.npy', np.array(kappa_perp))
+    np.save(file_name_output+str(step_size/10**11)+'_kappa_para.npy', np.array(kappa_para))
--- comparison/sim.py
+++ comparison/sim.py
@@ -1,4 +1,9 @@
 from crpropa_sim import *
 import numpy as np
 
-crpropa_sim()
+step_sizes = np.logspace(10**10, 10**12, 4)
+crpropa_sim(10**11)
+crpropa_sim(10**12)
+
+analyze_agn(10**11, 'sim_result_ana_')
+analyze_agn(10**12, 'sim_result_ana_')
