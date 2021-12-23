cbb8d9f45bec8d313a3b78bfbc5633e607447667
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-23 13:18:33 +0100

    created member functions to analyze the crpropa simulation results
    Former-commit-id: c4866730d4580638c872871b04533bf82a641a6c

--- comparison/crpropa_sim.py
+++ comparison/crpropa_sim.py
@@ -21,7 +21,7 @@ class CRPropa:
         self.traj_max = traj_max
 
 
-    def crpropa_sim(self, ):
+    def crpropa_sim(self):
         sim = crp.ModuleList()
 
         # point source settings
@@ -59,49 +59,49 @@ class CRPropa:
         sim.run(source, number_particles, True)
 
 
-def load_data(x):
-    dataI = pd.read_csv(x, names=['D', 'SN', 'ID', 'E', 'X', 'Y', 'Z', 'Px', 'Py', 'Pz', 'SN0', 'P0x', 'P0y', 'P0z', 'SN1'], delimiter='\t', comment='#', usecols=["D", "X", "Y", "Z", "\
-SN"])
+    def load_data(self, x):
+        dataI = pd.read_csv(x, names=['D', 'SN', 'ID', 'E', 'X', 'Y', 'Z', 'Px', 'Py', 'Pz', 'SN0', 'P0x', 'P0y', 'P0z', 'SN1'], delimiter='\t', comment='#', usecols=["D", "X", "Y", "Z", "\
+    SN"])
 
-    ### Convert data from Mpc to meters                                            
-    dataI.X = dataI.X * Mpc
-    dataI.Y = dataI.Y * Mpc
-    dataI.Z = dataI.Z * Mpc
-    dataI.D = dataI.D * Mpc
-    ### Calculate Diffusion Coefficients
-    dataI['X2D'] = dataI.X**2 / (dataI.D) * c / 2.
-    dataI['Y2D'] = dataI.Y**2 / (dataI.D) * c / 2.
-    dataI['Z2D'] = dataI.Z**2 / (dataI.D) * c / 2.
-    dataI['R2D'] = (dataI.X**2+dataI.Y**2+dataI.Z**2)**0.5
-    ### Distance                                                            
-    dataI.D = dataI.D
-    print(len(dataI.D.values.tolist()))
-    
-    dataI = dataI.sort_values('D')
-    return dataI
-
-
-def diffusion_coefficients(data):
-    kappa_xx = []
-    kappa_yy = []
-    kappa_zz = []
-    kappa_rr = []
-    L = return_L(data)
-    for l in L:
-        dataI = data[data['D'] == l]
-        kappa_xx.append(np.mean(dataI.X2D.values + dataI.Y2D.values)/2.0)
-        kappa_zz.append(np.mean(dataI.Z2D.values))
-    return kappa_xx, kappa_zz
-
-def return_L(dataI):
-    L = list(set(dataI.D.values.tolist()))
-    return sorted(L)
+        ### Convert data from Mpc to meters                                            
+        dataI.X = dataI.X * Mpc
+        dataI.Y = dataI.Y * Mpc
+        dataI.Z = dataI.Z * Mpc
+        dataI.D = dataI.D * Mpc
+        ### Calculate Diffusion Coefficients
+        dataI['X2D'] = dataI.X**2 / (dataI.D) * c / 2.
+        dataI['Y2D'] = dataI.Y**2 / (dataI.D) * c / 2.
+        dataI['Z2D'] = dataI.Z**2 / (dataI.D) * c / 2.
+        dataI['R2D'] = (dataI.X**2+dataI.Y**2+dataI.Z**2)**0.5
+        ### Distance                                                            
+        dataI.D = dataI.D
+        print(len(dataI.D.values.tolist()))
+        
+        dataI = dataI.sort_values('D')
+        return dataI
 
 
-def analyze_agn(step_size, file_name_output):
-    dataLin = load_data(file_name+str(step_size/10**11)+'.txt')
-    kappa_perp, kappa_para = diffusion_coefficients(dataLin)
-    L = return_L(dataLin)
-    np.save(file_name_output+str(step_size/10**11)+'_d', np.array(L))
-    np.save(file_name_output+str(step_size/10**11)+'_kappa_perp.npy', np.array(kappa_perp))
-    np.save(file_name_output+str(step_size/10**11)+'_kappa_para.npy', np.array(kappa_para))
+    def diffusion_coefficients(self, data):
+        kappa_xx = []
+        kappa_yy = []
+        kappa_zz = []
+        kappa_rr = []
+        L = self.return_L(data)
+        for l in L:
+            dataI = data[data['D'] == l]
+            kappa_xx.append(np.mean(dataI.X2D.values + dataI.Y2D.values)/2.0)
+            kappa_zz.append(np.mean(dataI.Z2D.values))
+        return kappa_xx, kappa_zz
+
+    def return_L(self, dataI):
+        L = list(set(dataI.D.values.tolist()))
+        return sorted(L)
+
+
+    def analyze_agn(self, step_size, file_name_output):
+        dataLin = self.load_data(file_name+str(step_size/10**11)+'.txt')
+        kappa_perp, kappa_para = self.diffusion_coefficients(dataLin)
+        L = self.return_L(dataLin)
+        np.save(file_name_output+str(step_size/10**11)+'_d', np.array(L))
+        np.save(file_name_output+str(step_size/10**11)+'_kappa_perp.npy', np.array(kappa_perp))
+        np.save(file_name_output+str(step_size/10**11)+'_kappa_para.npy', np.array(kappa_para))
