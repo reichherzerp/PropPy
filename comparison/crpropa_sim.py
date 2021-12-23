0d6fdd0686d008b2a162c657e9d52b2fb763c4fc
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-23 13:37:51 +0100

    finished crpropa simulation class
    Former-commit-id: 924ec62cd32b18f29f5bf421d54489945efb598f

--- comparison/crpropa_sim.py
+++ comparison/crpropa_sim.py
@@ -1,27 +1,23 @@
 import crpropa as crp
 import numpy as np
 import pandas as pd
-pc = 3.086*10**16
-Mpc = 10**6*pc
-c = 2.99792*10**8
-N_time = 100
-number_particles = 10**3
-N = number_particles
-file_name = 'data/sim_result_'
+
 
 class CRPropa:
-    def __init__(self, step_size = 10**11, traj_max = 10**16):
+    def __init__(self, step_size = 10**11, traj_max = 10**14):
         # all simulation parameters
         self.energy = 3*10**16*crp.eV
         self.n_obs = 100
-        self.brms = 10**6*crp.muG
+        self.n_particles = 10**3
+        self.brms = crp.gauss
         self.l_max = 5*10**11 # [m]
         self.l_min = 5*10**9 # [m]
         self.step_size = step_size
         self.traj_max = traj_max
+        self.file_name = 'data/raw_data/sim_result_'
 
 
-    def crpropa_sim(self):
+    def sim(self):
         sim = crp.ModuleList()
 
         # point source settings
@@ -44,7 +40,7 @@ class CRPropa:
         sim.add(maxTra)
 
         # output
-        output = crp.TextOutput(file_name+str(self.step_size/10**11)+'.txt', crp.Output.Trajectory3D)
+        output = crp.TextOutput(self.file_name+str(self.step_size/10**11)+'.txt', crp.Output.Trajectory3D)
         output.enable(output.SerialNumberColumn)
 
         # observer
@@ -56,22 +52,21 @@ class CRPropa:
 
         # run simulation                                               
         sim.setShowProgress(True)
-        sim.run(source, number_particles, True)
+        sim.run(source, self.n_particles, True)
 
 
     def load_data(self, x):
-        dataI = pd.read_csv(x, names=['D', 'SN', 'ID', 'E', 'X', 'Y', 'Z', 'Px', 'Py', 'Pz', 'SN0', 'P0x', 'P0y', 'P0z', 'SN1'], delimiter='\t', comment='#', usecols=["D", "X", "Y", "Z", "\
-    SN"])
+        dataI = pd.read_csv(x, names=['D', 'SN', 'ID', 'E', 'X', 'Y', 'Z', 'Px', 'Py', 'Pz', 'SN0', 'SN1'], delimiter='\t', comment='#', usecols=["D", "X", "Y", "Z", "SN"])
 
         ### Convert data from Mpc to meters                                            
-        dataI.X = dataI.X * Mpc
-        dataI.Y = dataI.Y * Mpc
-        dataI.Z = dataI.Z * Mpc
-        dataI.D = dataI.D * Mpc
+        dataI.X = dataI.X * 10**6*crp.pc
+        dataI.Y = dataI.Y * 10**6*crp.pc
+        dataI.Z = dataI.Z * 10**6*crp.pc
+        dataI.D = dataI.D * 10**6*crp.pc
         ### Calculate Diffusion Coefficients
-        dataI['X2D'] = dataI.X**2 / (dataI.D) * c / 2.
-        dataI['Y2D'] = dataI.Y**2 / (dataI.D) * c / 2.
-        dataI['Z2D'] = dataI.Z**2 / (dataI.D) * c / 2.
+        dataI['X2D'] = dataI.X**2 / (dataI.D) * crp.c_light / 2.
+        dataI['Y2D'] = dataI.Y**2 / (dataI.D) * crp.c_light / 2.
+        dataI['Z2D'] = dataI.Z**2 / (dataI.D) * crp.c_light / 2.
         dataI['R2D'] = (dataI.X**2+dataI.Y**2+dataI.Z**2)**0.5
         ### Distance                                                            
         dataI.D = dataI.D
@@ -83,9 +78,7 @@ class CRPropa:
 
     def diffusion_coefficients(self, data):
         kappa_xx = []
-        kappa_yy = []
         kappa_zz = []
-        kappa_rr = []
         L = self.return_L(data)
         for l in L:
             dataI = data[data['D'] == l]
@@ -93,13 +86,14 @@ class CRPropa:
             kappa_zz.append(np.mean(dataI.Z2D.values))
         return kappa_xx, kappa_zz
 
+
     def return_L(self, dataI):
         L = list(set(dataI.D.values.tolist()))
         return sorted(L)
 
 
     def analyze_agn(self, step_size, file_name_output):
-        dataLin = self.load_data(file_name+str(step_size/10**11)+'.txt')
+        dataLin = self.load_data(self.file_name+str(step_size/10**11)+'.txt')
         kappa_perp, kappa_para = self.diffusion_coefficients(dataLin)
         L = self.return_L(dataLin)
         np.save(file_name_output+str(step_size/10**11)+'_d', np.array(L))
--- comparison/sim.py
+++ comparison/sim.py
@@ -1,9 +1,9 @@
 from crpropa_sim import *
 import numpy as np
 
+
 step_sizes = np.logspace(10, 13, 8)
 for step_size in step_sizes:
-    crpropa_sim(step_size = step_size)
-
-for step_size in step_sizes:
-    analyze_agn(step_size, 'data/sim_result_ana_')
+    crp = CRPropa(step_size = step_size)
+    crp.sim()
+    crp.analyze_agn(step_size, 'data/sim_result_ana_')
