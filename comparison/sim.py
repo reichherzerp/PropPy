927ce9a335d2edd2f429dd278b24a8646e2f5d11
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-26 09:21:04 +0100

    reverse list of steps to be fast initially in comparison
    Former-commit-id: 0925a8c1daeb8db1c042a8d3a9f3ba8597d0b3e6

--- comparison/sim.py
+++ comparison/sim.py
@@ -4,8 +4,8 @@ from crpropa_sim import *
 import numpy as np
 
 
-step_sizes = np.logspace(10, 13, 8)
+step_sizes = np.logspace(10, 13, 8)[::-1]
 for step_size in step_sizes:
-    crp = CRPropa(step_size = step_size)
+    crp = CRPropa(step_size = step_size, traj_max = 10**14)
     crp.sim()
     crp.analyze(step_size, 'data/sim_result_crp_')
