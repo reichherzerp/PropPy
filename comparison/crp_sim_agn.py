5ae0f60fcb3ae5f8c71337577f497451ed31089d
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-29 10:42:06 +0100

    described analytical theory that underlies the comparison between numerics and simulation
    Former-commit-id: d185f6dbc50638d13e86aa4a49b07ba28deb9fc6

---
+++ comparison/crp_sim_agn.py
@@ -0,0 +1,25 @@
+# First, activate CRPropa environment and then run this current script
+
+from crpropa_sim import *
+import numpy as np
+import time
+
+step_sizes = np.logspace(10, 14, 15)[::-1]
+df_sim_data = pd.DataFrame(columns=('step_size', 'time', 'kappa'))
+
+for i, step_size in enumerate(step_sizes):
+    crp = CRPropa(step_size = step_size, traj_max = 10**17)
+    start_time = time.process_time()
+    crp.sim()
+    time_needed = time.process_time() - start_time
+    
+    kappa = crp.analyze(step_size, 'data/sim_result_crp_')
+
+    df_sim_data.loc[i] = [step_size, time_needed, kappa]
+
+
+# save simulation result
+file_name_results = 'data/crp_sim_data.pkl'
+df_sim_data.to_pickle(file_name_results)
+print(df_sim_data)
+    
