e6b7f03a16dfdcbb636eb8e800fc2df51ade3302
Patrick Reichherzer <patrick.reichherzer@rub.de>
2021-12-30 12:25:58 +0100

    updated all analyzing scripts with comparison between CK and BP
    Former-commit-id: 3ac8347d30941ebe803b9c6956ad1930c9f8ed74

--- comparison/crp_sim_compact_sources.py
+++ comparison/crp_sim_compact_sources.py
@@ -4,13 +4,13 @@ from crpropa_sim import *
 import numpy as np
 import time
 
-step_sizes = np.logspace(12, 14, 15)[::-1]
+step_sizes = np.logspace(10, 15, 15)[::-1]
 df_sim_data = pd.DataFrame(columns=('step_size', 'time', 'kappa', 'kappa_err'))
 path = 'compact_sources/'
-prop_module = 'CK'
+prop_module = 'BP'
 
 for i, step_size in enumerate(step_sizes):
-    crp = CRPropa(step_size = step_size, traj_max = 10**15, path = path, prop_module = prop_module)
+    crp = CRPropa(step_size = step_size, traj_max = 10**17, path = path, prop_module = prop_module)
     start_time = time.process_time()
     crp.sim()
     time_needed = time.process_time() - start_time
