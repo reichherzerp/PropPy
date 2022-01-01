corrected scaling
Former-commit-id: 789618c3e5796b4e2144ad8ddca6b611fe9ad3ee

comparison/crp_sim_test.py
@@ -8,10 +8,10 @@ step_sizes = np.logspace(10, 15, 15)[::-1]
 df_sim_data = pd.DataFrame(columns=('step_size', 'time', 'kappa', 'kappa_err'))
 path = 'compact_sources/'
 prop_module = 'SDE'
-kappa = 1.59*10**23 # [m^2/s]
+kappa_theory = 1.59*10**23 # [m^2/s]
 
 for i, step_size in enumerate(step_sizes[:10]):
-    crp = CRPropa(step_size = step_size, traj_max = 10**17, path = path, prop_module = prop_module, kappa = kappa)
+    crp = CRPropa(step_size = step_size, traj_max = 10**17, path = path, prop_module = prop_module, kappa = kappa_theory)
     start_time = time.process_time()
     crp.sim()
     time_needed = time.process_time() - start_time