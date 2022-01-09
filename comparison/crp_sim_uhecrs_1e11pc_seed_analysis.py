c611da64b710903cf54e6cc9503e2140f8bf38bb
Patrick Reichherzer <patrick.reichherzer@rub.de>
2022-01-09 15:51:05 +0100

    added script to simulate uhecrs
    Former-commit-id: 5db6bae7993c9f3cc185d6c70e06b0742ff8a060

--- comparison/crp_sim_uhecrs_1e11pc_seed_analysis.py
+++ comparison/crp_sim_uhecrs_1e11pc_seed_analysis.py
@@ -5,17 +5,17 @@ import numpy as np
 import time
 from pathlib import Path
 
-path = 'compact_sources_1e17m_seed_analysis/'
-path_figs = 'compact_sources_1e17m_seed_analysis/figures'
-path_data = 'compact_sources_1e17m_seed_analysis/data'
-path_data_raw = 'compact_sources_1e17m_seed_analysis/data/raw_data'
+path = 'uhecrs_1e11pc_seed_analysis/'
+path_figs = 'uhecrs_1e11pc_seed_analysis/figures'
+path_data = 'uhecrs_1e11pc_seed_analysis/data'
+path_data_raw = 'uhecrs_1e11pc_seed_analysis/data/raw_data'
 Path(path_figs).mkdir(parents=True, exist_ok=True)
 Path(path_data).mkdir(parents=True, exist_ok=True)
 Path(path_data_raw).mkdir(parents=True, exist_ok=True)
 
 step_size = 10**10 # [m]
 kappa_theory = 1.59*10**23 # [m^2/s]
-nr_seeds = 100
+nr_seeds = 5
 
 prop_module = 'BP'
 
@@ -25,7 +25,7 @@ file_name_results = path + 'data/crp_sim_data_'+prop_module+'_seed_'+'.pkl'
 df_sim_data = pd.DataFrame(columns=('seed', 'time', 'kappa', 'kappa_err'))
 
 for i, seed in enumerate(range(nr_seeds)):
-    crp = CRPropa(step_size = step_size, l_min = 5*10**9, traj_max = 10**17, path = path, prop_module = prop_module, kappa = kappa_theory, turbulence_method = 'PW', seed_study = True, random_seed = seed)
+    crp = CRPropa(step_size = step_size, l_min = 5*10**9, traj_max = 3e21, path = path, prop_module = prop_module, kappa = kappa_theory, turbulence_method = 'PW', seed_study = True, random_seed = seed)
     start_time = time.process_time()
     crp.sim()
     time_needed = time.process_time() - start_time
