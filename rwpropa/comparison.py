5be10be7c08b6e11d3935088831c25f983deb46b
Patrick Reichherzer <patrick.reichherzer@rub.de>
2022-01-28 19:06:42 +0100

    corrected xmin in running diff plot
    Former-commit-id: 79138821880804accad61c5e24bc49812d1a61a4

--- rwpropa/comparison.py
+++ rwpropa/comparison.py
@@ -124,12 +124,7 @@ class Comparison():
         kappas_bp_grid = []
         kappas_sde = []
 
-        if self.proppy_unit == 'km':
-            plt.xlim([0.5*self.step_sizes[0]*10**2, d_theory[-1]*1.5])
-        if self.proppy_unit == 'pc':
-            plt.xlim([0.5*self.step_sizes[0]*3.086*10**16, d_theory[-1]*1.5])
-        else: 
-            plt.xlim([0.5*self.step_sizes[0], d_theory[-1]*1.5])
+        plt.xlim([0.5*self.step_sizes[-1], d_theory[-1]*1.5])
         for i, step_size in enumerate(self.step_sizes):
             color = plt.cm.viridis(np.linspace(0, 1, len(self.step_sizes))[i])
             n_max = -1
