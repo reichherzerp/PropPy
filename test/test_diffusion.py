a878c4bdc021bfb9c92b1c0074ddfb7a967c2ea8
Patrick Reichherzer <patrick.reichherzer@rub.de>
2022-02-02 09:52:56 +0100

    init for test coverage

---
+++ test/test_diffusion.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import proppy.simulation as simulation
+import proppy.observer as observer
+import proppy.source as source
+import proppy.propagator as propagator
+import proppy.magnetic_field as mag_field
+
